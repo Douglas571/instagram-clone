647d4a7725b964272df3ba845d9e4bc357a90b9b
Douglas Socorro <douglassocorro.work@gmail.com>
2023-08-22 11:04:48 -0400

    Fixed some minor details in layout.

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,13 @@
-import { Box, Paper, Stack, TextField, Button, Divider, Typography } from '@mui/material';
+import {
+  Box,
+  Paper,
+  Stack,
+  TextField,
+  Button,
+  Divider,
+  Typography,
+  Link,
+} from '@mui/material';
 
 export default function Login(): JSX.Element {
   return (
@@ -12,27 +21,37 @@ export default function Login(): JSX.Element {
     >
       <Box
         sx={{
-          maxWidth: '400px',
+          maxWidth: '500',
           display: 'flex',
           flexDirection: 'column',
           alignContent: 'center',
           justifyContent: 'center',
         }}
       >
-        <Paper sx={{ p: 5 }} variant="outlined">
+        <Paper sx={{ p: 5, mb: 2 }} variant="outlined">
           <Box sx={{ mb: 3, textAlign: 'center' }}>PicSpot</Box>
-          <Stack gap={3}>
+          <Stack gap={2}>
             <TextField placeholder="Phone, nickname or email" />
             <TextField placeholder="Password" />
-            <Button variant="contained">Sing in</Button>
-            <Divider></Divider>
-            <Typography sx={{ textAlign: 'center' }}>
-              Do you forgot your password?
-            </Typography>
+            <Button variant="contained" disableRipple>
+              Sing in
+            </Button>
           </Stack>
+          <Divider sx={{ mt: 3, mb: 3 }}></Divider>
+          <Typography sx={{ textAlign: 'center' }}>
+            <Link href="/recover" sx={{ textDecoration: 'none' }}>
+              Do you forgot your password?
+            </Link>
+          </Typography>
         </Paper>
+
         <Paper variant="outlined" sx={{ p: 3, textAlign: 'center' }}>
-          <Typography>don't you have an account? Sing up</Typography>
+          <Typography>
+            don't you have an account?{' '}
+            <Link href="/singup" sx={{ textDecoration: 'none' }}>
+              Sing up
+            </Link>
+          </Typography>
         </Paper>
       </Box>
     </Box>
